d4bbc9b2f60e832777d257cfb97f902d046d6911
zoomkoding <zoomkoding@gmail.com>
2021-08-01 00:25:34 +0900

    Updates

---
+++ precache-manifest.0bb022a961c599811947ce096e416bfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac39d859055701f18ac64d45b0b8be53",
+    "url": "/3d-book/index.html"
+  },
+  {
+    "revision": "1526248ab832a476a612",
+    "url": "/3d-book/static/css/main.a1e07f1b.chunk.css"
+  },
+  {
+    "revision": "d8eb47a05572ed0205f1",
+    "url": "/3d-book/static/js/2.27261029.chunk.js"
+  },
+  {
+    "revision": "22411e8f7cffd1485b63ac0e6444c829",
+    "url": "/3d-book/static/js/2.27261029.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1526248ab832a476a612",
+    "url": "/3d-book/static/js/main.738449b8.chunk.js"
+  },
+  {
+    "revision": "4da3e1c7098802994a4a",
+    "url": "/3d-book/static/js/runtime-main.e7579e59.js"
+  }
+]);
